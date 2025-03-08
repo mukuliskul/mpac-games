b64f6602a82d40646af851a3552202a6584b8b3a
Mukul Sharma <169689349+mukul-mpac@users.noreply.github.com>
2025-03-07 23:14:25 -0500

    add base page for enroll and leaderboard

--- src/app/games/[name]/enroll/page.tsx
+++ src/app/games/[name]/enroll/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-export default function GamePage({
-	params,
-}: Readonly<{
-	params: { name: string };
-}>) {
-	const name: string = params.name;
+import { use } from "react";
 
-	return (
-		<div className="max-w-2xl mx-auto p-6">
-			<h1 className="text-3xl font-bold">Enroll for {name}</h1>
-		</div>
-	);
+export default function Enroll({
+  params,
+}: Readonly<{
+  params: Promise<{ name: string }>;
+}>) {
+  const { name } = use(params);
+
+  return (
+    <div className="max-w-2xl mx-auto p-6">
+      <h1 className="text-3xl font-bold">Enroll for {name}</h1>
+    </div>
+  );
 }
