ad394fa2cb27e3bd29263812ead0886d4f2743d6
Mukul Sharma <169689349+mukul-mpac@users.noreply.github.com>
2025-03-07 22:33:46 -0500

    add /games as default redirect

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/games',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
