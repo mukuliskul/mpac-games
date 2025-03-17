fa4f635296b0068cfb8bb8713a3523250c90da05
Mukul Sharma <169689349+mukul-mpac@users.noreply.github.com>
2025-03-17 13:41:42 -0400

    endpoint to get enrollments by game session

--- src/app/api/enroll/route.ts
+++ src/app/api/enroll/route.ts
@@ -1,5 +1,26 @@
 import { supabase } from "@/lib/supabase";
 
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const sessionId = url.searchParams.get("sessionId");
+
+  if (!sessionId) {
+    return new Response("Missing required fields", { status: 400 });
+  }
+
+  const { data, error } = await supabase
+    .from("enrollments")
+    .select("username")
+    .eq("game_session_id", sessionId);
+
+  if (error) {
+    console.error("Error fetching enrollments", error);
+    return new Response("Error fetching enrollments", { status: 500 });
+  }
+
+  return new Response(JSON.stringify(data), { status: 200 });
+}
+
 export async function POST(request: Request) {
   const { sessionId, name } = await request.json();
 
