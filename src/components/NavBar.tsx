cb19ceb93b36fc511b529a6b5c8f81efcd90f736
Mukul Sharma <169689349+mukul-mpac@users.noreply.github.com>
2025-03-17 21:32:55 -0400

    highlight current path

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import React, { useState } from 'react';
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const router = useRouter();
-
+  const pathname = usePathname();
   const navigateTo = (path: string) => {
     router.push(path);
   };
@@ -14,6 +14,8 @@ const NavBar = () => {
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
+
+  const isActive = (path: string) => pathname === path;
 
   return (
     <nav className="bg-gradient-to-r from-blue-800 to-indigo-900 text-white p-4 shadow-md sticky top-0 z-50 border-b border-indigo-600">
@@ -32,13 +34,13 @@ const NavBar = () => {
         <div className="hidden md:flex space-x-6">
           <button
             onClick={() => navigateTo('/games')}
-            className="text-sm uppercase hover:text-yellow-500 transition duration-300"
+            className={`text-sm uppercase hover:text-yellow-500 transition duration-300 ${isActive('/games') ? 'text-yellow-500' : ''}`}
           >
             Games
           </button>
           <button
             onClick={() => navigateTo('/leaderboard')}
-            className="text-sm uppercase hover:text-yellow-500 transition duration-300"
+            className={`text-sm uppercase hover:text-yellow-500 transition duration-300 ${isActive('/leaderboard') ? 'text-yellow-500' : ''}`}
           >
             Leaderboard
           </button>
@@ -57,13 +59,13 @@ const NavBar = () => {
         <div className="md:hidden flex flex-col space-y-4 mt-4 bg-gradient-to-r from-blue-800 to-indigo-900 p-4 rounded-lg border-t border-indigo-600 shadow-md transition-all ease-in-out duration-300">
           <button
             onClick={() => navigateTo('/games')}
-            className="text-white text-lg uppercase hover:text-yellow-500 transition duration-300"
+            className={`text-white text-lg uppercase hover:text-yellow-500 transition duration-300 ${isActive('/games') ? 'text-yellow-500' : ''}`}
           >
             Games
           </button>
           <button
             onClick={() => navigateTo('/leaderboard')}
-            className="text-white text-lg uppercase hover:text-yellow-500 transition duration-300"
+            className={`text-white text-lg uppercase hover:text-yellow-500 transition duration-300 ${isActive('/leaderboard') ? 'text-yellow-500' : ''}`}
           >
             Leaderboard
           </button>
