bd84b6f1487040ca023625a71a5fe459b3ad3180
Mukul Sharma <169689349+mukul-mpac@users.noreply.github.com>
2025-06-04 17:48:47 -0400

    chore: add bracket viewer deps

--- src/app/api/test/generateFirstRound/route.ts
+++ src/app/api/test/generateFirstRound/route.ts
@@ -4,7 +4,7 @@ import { generateFirstRound } from "@/lib/generateBracket";
 export async function GET() {
   try {
     // TODO: should be triggered by a cron job when enrollment end date is reached
-    await generateFirstRound("a8345bed-43f0-40b4-8b7f-01b840bdaa56", "2025-05-12");
+    await generateFirstRound("d6f8850b-4cc5-4d81-9711-9e7dccfa5796", "2025-05-12");
     return NextResponse.json({ success: true });
   } catch (error: unknown) {
     const message = error instanceof Error ? error.message : 'Unknown error';
--- src/app/api/test/generateNextRound/route.ts
+++ src/app/api/test/generateNextRound/route.ts
@@ -4,7 +4,7 @@ import { generateNextRound } from "@/lib/generateBracket";
 export async function GET() {
   try {
     // TODO: should be triggered by a trigger when winner is added for all games in that round
-    await generateNextRound("a8345bed-43f0-40b4-8b7f-01b840bdaa56", "2025-05-19");
+    await generateNextRound("d6f8850b-4cc5-4d81-9711-9e7dccfa5796", "2025-05-19");
     return NextResponse.json({ success: true });
   } catch (error: unknown) {
     const message = error instanceof Error ? error.message : 'Unknown error';
