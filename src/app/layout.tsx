364ac8c1bf479017ecf8efad4886b0ee8772beae
Mukul Sharma <169689349+mukul-mpac@users.noreply.github.com>
2025-06-22 17:24:43 -0400

    fix: vertical scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full overflow-hidden">
+    <html lang="en" className="h-full overflow-y-auto">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
       >
