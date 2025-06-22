0a134544fba26c82f9ffd3a07196719c71d98a5c
Mukul Sharma <169689349+mukul-mpac@users.noreply.github.com>
2025-06-22 17:16:02 -0400

    feat: use EDT/EST in countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,28 +1,32 @@
+import { parseNYDateString } from "@/lib/date";
 import React, { useEffect, useState } from "react";
+import { DateTime } from "luxon";
 
 export const Countdown = ({ targetDate }: { targetDate: string }) => {
   const [timeLeft, setTimeLeft] = useState("");
 
+
   useEffect(() => {
-    const target = new Date(targetDate).getTime();
+    const target = parseNYDateString(targetDate);
 
     const updateCountdown = () => {
-      const now = new Date().getTime();
-      const difference = target - now;
+      const now = DateTime.local();
 
-      if (difference <= 0) {
+      if (target <= now) {
         setTimeLeft("Enrollment closed");
         return;
       }
 
-      const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
-      const minutes = Math.floor((difference / (1000 * 60)) % 60);
-      const seconds = Math.floor((difference / 1000) % 60);
+      // Get difference as Duration
+      const diff = target.diff(now, ['days', 'hours', 'minutes', 'seconds']).toObject();
 
-      setTimeLeft(
-        `${days}d ${hours}h ${minutes}m ${seconds}s`
-      );
+      // Round values down
+      const days = Math.floor(diff.days ?? 0);
+      const hours = Math.floor(diff.hours ?? 0);
+      const minutes = Math.floor(diff.minutes ?? 0);
+      const seconds = Math.floor(diff.seconds ?? 0);
+
+      setTimeLeft(`${days}d ${hours}h ${minutes}m ${seconds}s`);
     };
 
     updateCountdown(); // initial call
