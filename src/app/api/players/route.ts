33f5f235535a9cb50203101719ed51edb5aaeb93
Mukul Sharma <169689349+mukul-mpac@users.noreply.github.com>
2025-06-26 09:06:57 -0400

    chore: order names in ascending order

--- src/app/api/players/route.ts
+++ src/app/api/players/route.ts
@@ -1,7 +1,7 @@
 import { supabase } from "@/lib/supabase";
 
 export async function GET() {
-  const { data, error } = await supabase.from("players").select("username");
+  const { data, error } = await supabase.from("players").select("username").order("username", { ascending: true });
 
   if (error) {
     return new Response("Error fetching data", { status: 500 });
